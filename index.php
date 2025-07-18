51a3d2eabf30a6ad98f308c0d22f1db2f4e5d3d0
lmerv <info@merveille.ch>
2025-07-18 11:36:32 +0200

    assets include

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php include_once './assets/layout/doctype.php' ?>
+
     <title>Page d'accueil</title>
     <link rel="stylesheet" href="./styles.css">
 </head>
